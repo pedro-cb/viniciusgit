cd94103c1e7384aee46aa0d544a6f79b04004f70
Vinícius <pedroviniciuscb1231@gmail.com>
2023-08-08 11:20:59 -0300

    Alteração no nome

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>É PEDROOOOOOO</p>
+        <p>Pedro Vinícius Campos de Barros</p>
         <a
           className="App-link"
           href="https://reactjs.org"
